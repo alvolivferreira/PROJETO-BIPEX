d4d550c00e42a835b0b28135c81dba99193c16c7
alvolivferreira <alvolivferreira@gmail.com>
2026-05-22 23:00:45 +0000

    BIPLEX

---
+++ teste.py
@@ -0,0 +1,53 @@
+#                                ***PROJETO BIPEX. ☠  ***
+
+mensagem = input("digite um negocio ae:")
+if mensagem == "oi":
+    exit ()
+nome = input("digite seu nome: ")
+idade = int(input(f"digite sua idade, {nome} : "))
+
+
+if idade <= 18:
+    print (f"Menor de idade, otaruuuoa de {idade} anus de idade")
+    exit()
+
+print ("Acesso liberado")
+
+
+locked = input("Digite oa cesso:")
+
+contador = 0    
+while contador < 3:
+    locked = input("Digite seu acesso:")
+    if locked == "2233":
+            print("PARABENS acesso liberaddo monomono")
+            print("agora voce pode")
+            print(f"Pega nos meus ovos, {nome}, voce e seus {idade} anos de idade")
+            break
+    else:
+        contador += 1
+        print ("you is nigga?")
+if contador == 3:
+            print ("ta achando que nasci ontem bro? you is nigga?")
+exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
