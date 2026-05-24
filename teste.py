567afa1c86a17b6ee89e71793518180cb3f346e0
alvolivferreira <alvolivferreira@gmail.com>
2026-05-24 18:42:22 +0000

    FASE 1 FINALIZADA

--- teste.py
+++ teste.py
@@ -1,40 +1,67 @@
 #                                ***PROJETO BIPEX. ☠  ***
 
-mensagem = input("digite um negocio ae:")
-if mensagem == "oi":
-    exit ()
-nome = input("digite seu nome: ")
-idade = int(input(f"digite sua idade, {nome} : "))
+from inputimeout import inputimeout, TimeoutOccurred
 
+oi = input("digita: ")
 
-if idade <= 18:
-    print (f"Menor de idade, otaruuuoa de {idade} anus de idade")
-    exit()
+if oi == "oi":
+    try:
+        mensagem = inputimeout(
+            prompt="digite um negocio ae: ",
+            timeout=15
+        )
+    except TimeoutOccurred:
+        exit()
 
-print ("Acesso liberado")
+    if mensagem == "oi":
+        exit()
 
+    nome = input("digite seu nome: ")
+    idade = int(input(f"digite sua idade, {nome}: "))
 
-locked = input("Digite oa cesso:")
+    if idade <= 18:
+        print(f"Menor de idade: {idade} anos")
+        exit()
 
-contador = 0    
-while contador < 3:
-    locked = input("Digite seu acesso:")
-    if locked == "2233":
-            print("PARABENS acesso liberaddo monomono")
+    print("Acesso liberado")
+
+    locked = input("Digite o acesso: ")
+
+    contador = 0
+
+    while contador < 3:
+        locked = input("Digite seu acesso: ")
+
+        if locked == "2233":
+            print("PARABÉNS acesso liberado")
             print("agora voce pode")
-            print(f"Pega nos meus ovos, {nome}, voce e seus {idade} anos de idade")
+            print(f"Bem-vindo {nome}, idade {idade}")
             break
-    else:
-        contador += 1
-        print ("you is nigga?")
-if contador == 3:
-            print ("ta achando que nasci ontem bro? you is nigga?")
-exit()
 
+        else:
+            contador += 1
+            print("Senha incorreta")
 
+    if contador == 3:
+        print("Número máximo de tentativas atingido")
+        exit()
 
+else:
+    usuarios = []
+    usuarios.extend(["Guilherme","baiano","juninho"])
 
+    print(usuarios)
 
+    usuarios.append(input("LISTA DE SABADO: "))
+    print(usuarios)
+
+    alertas = []
+    alerta = input("agora vamos a lista de alerta: ")
+    alertas.extend([alerta, "alerta1", "alerta2", "alerta3"])
+
+    print(alertas)
+    for usuario in usuarios:
+        print (usuario)
 
 
 
